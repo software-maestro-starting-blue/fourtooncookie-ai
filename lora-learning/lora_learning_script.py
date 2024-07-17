ad25b26a2084a24b6d84960999ac7c216a1f5583
JuneBridge <junebridge03@g.skku.edu>
2024-07-17 22:23:04 +0900

    init: 실행 로직과 테스트 로직을 분리

--- lora-learning/lora_learning_script.py
+++ lora-learning/lora_learning_script.py
@@ -1,10 +1,10 @@
 import subprocess
 
-args = {}
 
-command = "accelerate launch --num_cpu_threads_per_process 1 train_network.py"
+def start_lora_learning(args):
+    command = "accelerate launch --num_cpu_threads_per_process 1 train_network.py"
 
-for key, value in args.items():
-    command += "\n --{} {}".format(key, value)
+    for key, value in args.items():
+        command += "\n --{} {}".format(key, value)
 
-subprocess.run(command)
+    subprocess.run(command, cwd="./sd-scripts")
---
+++ lora-learning/test_lora_learning.py
@@ -0,0 +1,5 @@
+from lora_learning_script import start_lora_learning
+
+
+def test1():
+    pass # TODO: 이런 식으로 여러 테스트를 만들어서 그에 대한 모델을 양산 후 검증
