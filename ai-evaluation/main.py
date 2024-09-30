378cdca31fcd4aea513ddcaa4beeed8ec5a3f247
JuneBridge <junebridge03@g.skku.edu>
2024-09-30 12:40:22 +0900

    feat: main.py의 로직을 구성한다.

--- ai-evaluation/main.py
+++ ai-evaluation/main.py
@@ -2,18 +2,54 @@ from openai import OpenAI
 
 from llm.generator.llm_generator import LLMGenerator
 from llm.generator.gpt4o_llm_generator import GPT4oLLMGenerator
+from llm.prompting.llm_prompting_service import LLMPromptingSerivce
+from llm.rating.llm_rating_service import LLMRatingService
+
+import json
 
 openai: OpenAI = OpenAI()
 
 llm_service: LLMGenerator = GPT4oLLMGenerator(openai)
 
+llm_prompting_service: LLMPromptingSerivce = None
+
+llm_rating_services: list[LLMRatingService] = []
+
 def main():
     # data를 가지고 온다.
+    f = open("input_data.json", "r")
+    input_datas = json.load(f)
+    f.close()
 
     # LLMService를 활용한 LLMPromptingService 통해 결과물을 생성한다.
+    data_sets = [
+        (input_data, llm_prompting_service.get_llm_result(input_data))
+        for input_data in input_datas
+    ]
     
     # 생성된 결과물을 LLMRatingService를 활용하여 평가한다.
-    pass
+    rating_results = [
+        [
+            llm_rating_service.get_rating_of_llm(user_input, llm_result)
+            for llm_rating_service in llm_rating_services
+        ]
+        for user_input, llm_result in data_sets
+    ]
+    
+    # data_sets와 rating_results를 합쳐서 결과를 만든다.
+    results = [
+        {
+            "user_input": user_input,
+            "llm_result": llm_result,
+            "ratings": ratings
+        }
+        for (user_input, llm_result), ratings in zip(data_sets, rating_results)
+    ]
+
+    # 결과를 json으로 저장한다.
+    f = open("rating_result.json", "w")
+    json.dump(results, f)
+    f.close()
 
 if __name__ == '__main__':
     main()
