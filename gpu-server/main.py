64f5d3484524165e4a946732b14553e24ed3d786
JuneBridge <junebridge03@g.skku.edu>
2024-10-01 15:57:46 +0900

    feat: s3 이미지 전송으로 로직 수정 & feat: sentry 적용 & feat: KeyboardInterrupt 적용

--- gpu-server/main.py
+++ gpu-server/main.py
@@ -1,56 +1,64 @@
 from asyncio import sleep
-import boto3
 import os
 import glob
-import base64
 import json
-from aws_sqs import receive_messages, delete_message, send_message
+from aws_sqs import receive_messages, delete_message
+from aws_s3 import upload_image_to_s3
 from sdxl_lora_runner import generate_image_sdxl_with_lora
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_DSN'],
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
-
-def get_image_from_dir(diary_id, grid_position):
-    folder_path = f"output/{diary_id}_{grid_position}"
-    png_files = glob.glob(os.path.join(folder_path, '*.png'))
-    if not png_files:
-        return None
-    
-    with open(f"output/{diary_id}_{grid_position}/{png_files[0]}", 'rb') as f:
-        image_bytes = f.read()
-        image_base64 = base64.b64encode(image_bytes).decode('utf-8')
-    
-    os.remove(f"output/{diary_id}_{grid_position}/{png_files[0]}")
-    return image_base64
 
 def extract_message(message_body):
     message = json.loads(message_body)
     return message['diaryId'], message['characterId'], message['prompt'], message['gridPosition']
 
+def get_image_path(diary_id, grid_position):
+    folder_path = f"output/{diary_id}_{grid_position}"
+    png_files = glob.glob(os.path.join(folder_path, '*.png'))
+    if not png_files:
+        return None
+    
+    return f"./output/{diary_id}_{grid_position}/{png_files[0]}"
+
+def delete_output_images():
+    for file in glob.glob('output/*'):
+        os.remove(file)
+
 def run():
     while True:
-        try:
-            sleep(3) # 3초동안 대기
+        sleep(3) # 3초동안 대기
 
-            messages = receive_messages()
+        messages = receive_messages()
 
-            if not messages:
-                continue
-
-            for message_body, message_receipt_handle in messages:
-                diary_id, character_id, prompt, grid_position = extract_message(message_body)
-
-                lora_model = f"lora/model_{character_id}.safetensors"
-                output_dir = f"output/{diary_id}_{grid_position}"
-
-                generate_image_sdxl_with_lora(lora_model, prompt, output_dir)
-                image_base64 = get_image_from_dir(diary_id, grid_position)
-
-                send_message(diary_id, grid_position, image_base64)
-                delete_message(message_receipt_handle)
-        except Exception as e:
-            print("An error occurred:", str(e))
-            sleep(10)
+        if not messages:
             continue
 
+        for message_body, message_receipt_handle in messages:
+            diary_id, character_id, prompt, grid_position = extract_message(message_body)
+
+            lora_model = f"lora/model_{character_id}.safetensors"
+            output_dir = f"output/{diary_id}_{grid_position}"
+
+            generate_image_sdxl_with_lora(lora_model, prompt, output_dir)
+            image_path = get_image_path(diary_id, grid_position)
+
+            upload_image_to_s3(image_path, diary_id, grid_position)
+
+            delete_message(message_receipt_handle)
+
 if __name__ == '__main__':
-    run()
+    try:
+        run()
+    except KeyboardInterrupt as e:
+        print("Shutting down...")
+    except Exception as e:
+        print("An error occurred:", str(e))
+        sentry_sdk.capture_exception(e)
+    finally:
+        delete_output_images()
