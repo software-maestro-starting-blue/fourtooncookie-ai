3ddd466cac9405bf9e6c73bbc22779b917bf2786
JuneBridge <junebridge03@g.skku.edu>
2024-10-03 15:20:02 +0900

    feat: ImageResponseSQS에 success와 failure 되는가를 확인하는 로직 추가

--- gpu-server/aws_sqs.py
+++ gpu-server/aws_sqs.py
@@ -1,9 +1,11 @@
 import boto3
 import os
+import json
 
 sqs_client = boto3.client('sqs', region_name='ap-northeast-2')
 
 SPRING_TO_STABLEDIFFUSION_QUEUE_URL= os.environ['SPRING_TO_STABLEDIFFUSION_QUEUE_URL']
+IMAGE_RESPONSE_QUEUE_URL = os.environ['IMAGE_RESPONSE_QUEUE_URL']
 
 NUMBER_OF_MESSAGES_TO_RECEIVE = 5
 
@@ -38,3 +40,33 @@ def delete_message(receipt_handle):
         print("Deleted message from queue.")
     except Exception as e:
         print("An error occurred while deleting message:", str(e))
+
+
+def send_image_success_message(diary_id: int, grid_position: int):
+    try:
+        sqs_client.send_message(
+            QueueUrl=IMAGE_RESPONSE_QUEUE_URL,
+            MessageBody=json.dumps({
+                'diaryId': diary_id,
+                'gridPosition': grid_position,
+                'status': True
+            })
+        )
+        print("Sent image success message to queue.")
+    except Exception as e:
+        print("An error occurred while sending image success message:", str(e))
+
+
+def send_image_failure_message(diary_id: int, grid_position: int):
+    try:
+        sqs_client.send_message(
+            QueueUrl=IMAGE_RESPONSE_QUEUE_URL,
+            MessageBody=json.dumps({
+                'diaryId': diary_id,
+                'gridPosition': grid_position,
+                'status': False
+            })
+        )
+        print("Sent image failure message to queue.")
+    except Exception as e:
+        print("An error occurred while sending image failure message:", str(e))
